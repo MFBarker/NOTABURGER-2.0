6b001e5323210150e1bff8a6c30a767ee991fd5c
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2023-02-16 16:25:51 -0700

    Telerik API Testing is added, Make sure if you need to test something you do it on here. Database IS NOT CONNECTED on Testing. - VALERIA ARENAS

---
+++ Testing/Models/ClientAPI.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Testing.Models
+{
+    public class ClientAPI
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+        static HttpClient client = new HttpClient();
+
+
+        // Initializes the HttpClient 
+        /// <summary>
+        /// Sets the base uRI for HTTP requests. Change the port number to the port used in ther server app is used
+        /// Sets the Accept header to "application/json". Setting this header tells the server to send data in JSON format
+        /// </summary>
+        /// <returns>async Task</returns>
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line
+            client.BaseAddress = new Uri("https://localhost:7162/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // GET request for a product
+        static async Task<ProductInfo>
+
+    }
+
+
+}
