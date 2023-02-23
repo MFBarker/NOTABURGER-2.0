9415a7f389ab8ddaba36008eb83ea43e22ae24a3
MFBarker <98473836+MFBarker@users.noreply.github.com>
2023-02-23 16:58:36 -0700

    Michelle - Implementing DAL, Testing Menu Code with Database, Fixing Navbar for Home Page Link

---
+++ NOTABURGER 2/Data/NotABurgerDAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NOTABURGER_2.Interfaces;
+using NOTABURGER_2.Models;
+
+namespace NOTABURGER_2.Data
+{
+    public class NotABurgerDAL : IDataAccessLayer
+    {
+        private ApplicationDbContext db;
+
+        public NotABurgerDAL(ApplicationDbContext indb)
+        {
+            db = indb;
+        }
+
+        public IEnumerable<MenuItem> GetMenu()
+        {
+            return db.MenuItems.ToList();
+        }
+    }
+}
