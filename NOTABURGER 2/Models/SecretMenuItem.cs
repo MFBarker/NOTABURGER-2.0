1eed758bfeed5f8dbc2d6bf2b2591dff99859e0d
MFBarker <98473836+MFBarker@users.noreply.github.com>
2023-03-06 23:36:16 -0700

    Michelle - Secret Menu code, Database Mostly done

---
+++ NOTABURGER 2/Models/SecretMenuItem.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NOTABURGER_2.Models
+{
+    public class SecretMenuItem
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public float soloPrice { get; set; }
+
+        [Required]
+        public string type { get; set; }
+
+        [Required]
+        public string? includes { get; set; }
+
+        [Required]
+        public string? allergens { get; set; }
+
+        [Required]
+        public string? imageSrc { get; set; }
+
+        SecretMenuItem() { }
+
+        SecretMenuItem(int id, string name, float soloPrice,string type, string? includes, string? allergens, string? imageSrc)
+        {
+            this.id = id;
+            this.name = name;
+            this.soloPrice = soloPrice;
+            this.type = type;
+            this.includes = includes;
+            this.allergens = allergens;
+            this.imageSrc = imageSrc;
+        }
+    }
+}
