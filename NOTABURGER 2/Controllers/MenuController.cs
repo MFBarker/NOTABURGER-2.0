24e95f168c29b0e38877a9cae8a9523674d9cb94
KyleXR <98604126+KyleXR@users.noreply.github.com>
2023-03-06 20:26:40 -0700

    Added Edit to MenuController - Kyle Reed

--- NOTABURGER 2/Controllers/MenuController.cs
+++ NOTABURGER 2/Controllers/MenuController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NOTABURGER_2.Interfaces;
+using NOTABURGER_2.Models;
 
 namespace NOTABURGER_2.Controllers
 {
     public class MenuController : Controller
     {
+        IDataAccessLayer dal;
+        public MenuController(IDataAccessLayer indal)
+        {
+            dal = indal;
+        }
         //[HttpPost]
         public IActionResult DUD()
         {
@@ -19,6 +26,29 @@ namespace NOTABURGER_2.Controllers
         public IActionResult SecretDUD()
         {
             return RedirectToAction("SecretMenu", "Home");
+        }
+
+        [HttpGet] //loading edit page
+        public IActionResult Edit(int? id)
+        {
+            MenuItem foundItem = dal.GetMenu().Where(g => g.id == id).FirstOrDefault();
+
+            dal.Edit(foundItem);
+            return View(foundItem);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(MenuItem m)
+        {
+            if (ModelState.IsValid)
+            {
+                dal.Edit(m);
+                TempData["success"] = m.name + " updated";
+                return RedirectToAction("Menu", "Menu");
+
+            }
+            return View();
         }
     }
 }
