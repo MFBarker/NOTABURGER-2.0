3e201d0605e94f5582c15bbccf32bc683eabff26
gwelty24 <100785532+gwelty24@users.noreply.github.com>
2023-03-06 19:45:50 -0700

    Grace Welty - Zip code Api complete

--- NOTABURGER 2/Models/Place.cs
+++ NOTABURGER 2/Models/Place.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
 
 namespace NOTABURGER_2.Models
 {
     public class Place : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        [JsonPropertyName("place name")]
+        public string PlaceName { get; set; }
+        public string Longitude { get; set; }
+        public string State { get; set; }
+        [JsonPropertyName("state abbreviation")]
+        public string StateAbbreviation { get; set; }
+        public string Latitude { get; set; }
     }
 }
